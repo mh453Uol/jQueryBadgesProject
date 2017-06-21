8a3be7e2367bfb32d30ff73e44c965fc07370896
mh453Uol <hussainmajid95@gmail.com>
2017-06-21 11:33:52 +0100

    Implemented ajax call to get badges and display them

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
-
-  // your code will go here
-
+  //Get Report Card
+  getBadges('https://www.codeschool.com/users/sergiocruz.json');
 });
+
+function getBadges(url){
+    $.ajax({
+    url: url,
+    dataType: 'jsonp',
+    success: function(response){
+      console.log(response);
+      displayCompletedBadges(response.courses.completed);
+      displayInProgressBadges(response.courses.in_progress);
+    },
+    error: function(resp){
+      alert('We could load the user badges. Sorry!');
+    }
+  })
+}
+
+function displayCompletedBadges(completed){
+  completed.forEach(function(element) {
+    $(".js-completed").append(displayBadge(element));
+  }, this);
+}
+
+function displayInProgressBadges(inProgress){
+  inProgress.forEach(function(element) {
+    $(".js-inprogress").append(displayBadge(element));
+  }, this);
+}
+
+function displayBadge(badge){
+  var badge = '<div class="course">' +
+                '<h3>'+badge.title+'</h3>'+
+                '<img src='+badge.badge+' alt='+badge.title+'>'+
+                '<a target="_blank" class="btn btn-primary" href='+badge.url+'>See Course</>'
+              '</div>'
+  return badge;
+}
